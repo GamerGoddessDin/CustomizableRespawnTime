45797894fffa5800df7f67aa2e0f3a7ac1b10915
GamerGoddessDin <gamer.goddess@gameplayersdream.com>
2017-07-09 05:55:21 -0500

    Initial Commit v0.75.25 Has a build error still.

---
+++ Config.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using Terraria;
+using Terraria.IO;
+using Terraria.ModLoader;
+
+namespace CustomRespawnTime
+{
+    public static class Config
+    {
+        public static bool UseOnyxSteelPref = true;
+		public static bool UsePresetTime = false;
+		public static bool UseMultiplier = false;
+        public static float RespawnSeconds = 10.0;
+		public static float RespawnMultiplier = 0.33;
+
+        //The file will be stored in "Terraria/ModLoader/Mod Configs/CustomRespawnTime.json"
+        static string ConfigPath = Path.Combine(Main.SavePath, "Mod Configs", "CustomRespawnTime.json");
+
+        static Preferences Configuration = new Preferences(ConfigPath);
+
+        public static void Load()
+        {
+            //Reading the config file
+            bool success = ReadConfig();
+
+            if(!success)
+            {
+                ErrorLogger.Log("Failed to read CustomRespawnTime's config file! Recreating config...");
+                CreateConfig();
+            }
+        }
+
+        //Returns "true" if the config file was found and successfully loaded.
+        static bool ReadConfig()
+        {
+            if(Configuration.Load())
+            {
+                Configuration.Get("UseOnyxSteelPref", ref UseOnyxSteelPref);
+                Configuration.Get("UsePresetTime", ref UsePresetTime);
+                Configuration.Get("UseMultiplier", ref UseMultiplier);
+                Configuration.Get("RespawnSeconds", ref RespawnSeconds);
+                Configuration.Get("RespawnMultiplier", ref RespawnMultiplier);
+                return true;
+            }
+            return false;
+        }
+
+        //It would make more sense to call this method SaveConfig(), but since we don't have an in-game editor or anything, this will only be called if a config file wasn't found or it's invalid.
+        static void CreateConfig()
+        {
+            Configuration.Clear();
+            Configuration.Put("UseOnyxSteelPref", UseOnyxSteelPref);
+            Configuration.Put("UsePresetTime", UsePresetTime);
+            Configuration.Put("UseMultiplier", UseMultiplier);
+            Configuration.Put("RespawnSeconds", RespawnSeconds);
+            Configuration.Put("RespawnMultiplier", RespawnMultiplier);
+            Configuration.Save();
+        }
+    }
+}
---
+++ CustomRespawnTime.cs
@@ -0,0 +1,46 @@
+using Terraria.ModLoader;
+using Terraria;
+using System;
+using Terraria.DataStructures;
+
+namespace CustomRespawnTime
+{
+	class CustomRespawnTime : Mod
+	{
+		public CustomRespawnTime()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+		}
+	}
+	public class CustomRespawnTimePlayer : ModPlayer
+	{
+		public override void Kill(double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource)
+		{
+			if(Config.UseOnyxSteelPref)
+			{
+				player.respawnTimer = 600;
+				return;
+			}
+			
+			if(Config.UsePresetTime)
+			{
+				player.respawnTimer = (int)(Config.RespawnSeconds * 60);
+				return;
+			}
+			
+			if(Config.UseMultiplier)
+			{
+				player.respawnTimer = (int)(player.respawnTimer * Config.RespawnMultiplier);
+			}
+		}
+	}
+	public override void Load()
+	{
+		Config.Load();
+	}
+}
