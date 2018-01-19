25dcadea3529d19bf3c922e0715e2b68722fd963
hamstar0 <webutek@gmail.com>
2018-01-19 15:01:36 -0800

    v0.77.0 * Added Hamstar's Helpers support for mod issue reporting and config editing

--- CustomRespawnTime.cs
+++ CustomRespawnTime.cs
@@ -2,44 +2,56 @@ using Terraria.ModLoader;
 using Terraria;
 using System;
 using Terraria.DataStructures;
+using System.IO;
 
-namespace CustomRespawnTime
-{
-	class CustomRespawnTime : Mod
-	{
-		public CustomRespawnTime()
-		{
-			Properties = new ModProperties()
-			{
+
+namespace CustomRespawnTime {
+	class CustomRespawnTime : Mod {
+		public static string GithubUserName { get { return "GamerGoddessDin"; } }
+		public static string GithubProjectName { get { return "CustomizableRespawnTime"; } }
+
+		public static string ConfigFileRelativePath {
+			get { return Config.SubConfigFolder + Path.DirectorySeparatorChar + Config.ConfigFileName; }
+		}
+
+		public static void ReloadConfigFromFile() {
+			if( Main.netMode != 0 ) {
+				throw new Exception( "Cannot reload configs outside of single player." );
+			}
+			Config.Load();
+		}
+
+
+		////////////////
+
+		public CustomRespawnTime() {
+			Properties = new ModProperties() {
 				Autoload = true,
 				AutoloadGores = true,
 				AutoloadSounds = true
 			};
 		}
-		public override void Load()
-		{
+		public override void Load() {
 			Config.Load();
 		}
 	}
-	public class CustomRespawnTimePlayer : ModPlayer
-	{
-		public override void Kill(double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource)
-		{
-			if(Config.UseOnyxSteelPref)
-			{
+
+
+
+	public class CustomRespawnTimePlayer : ModPlayer {
+		public override void Kill( double damage, int hitDirection, bool pvp, PlayerDeathReason damageSource ) {
+			if( Config.UseOnyxSteelPref ) {
 				player.respawnTimer = 600;
 				return;
 			}
-			
-			if(Config.UsePresetTime)
-			{
-				player.respawnTimer = (int)(Config.RespawnSeconds * 60);
+
+			if( Config.UsePresetTime ) {
+				player.respawnTimer = (int)( Config.RespawnSeconds * 60 );
 				return;
 			}
-			
-			if(Config.UseMultiplier)
-			{
-				player.respawnTimer = (int)(player.respawnTimer * Config.RespawnMultiplier);
+
+			if( Config.UseMultiplier ) {
+				player.respawnTimer = (int)( player.respawnTimer * Config.RespawnMultiplier );
 			}
 		}
 	}
